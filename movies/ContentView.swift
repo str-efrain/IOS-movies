8b775ce1ea3121159225b24b403152c9d59c5644
efrain mouton <efrain.mouton@student.vives.be>
2025-11-18 10:07:30 +0100

    Initial Commit

---
+++ movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movies
+//
+//  Created by Efrain Mouton on 18/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Efrain Mouton on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
